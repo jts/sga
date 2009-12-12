9ebfa566be40e1f204cfa568c783319ebe4e3f90
Jared Simpson <js18@sanger.ac.uk>
2009-12-12 01:11:41 +0000

    Removed hash test code as its not smaller or faster than using std::map. The vertex finds are not a bottleneck.

--- src/Bigraph/Bigraph.h
+++ src/Bigraph/Bigraph.h
@@ -13,7 +13,6 @@
 #include <stdio.h>
 #include <vector>
 #include <map>
-#include <ext/hash_map>
 #include "GraphCommon.h"
 #include "Vertex.h"
 #include "Edge.h"
@@ -21,25 +20,8 @@
 //
 // Typedefs
 //
-namespace __gnu_cxx {
 
-   template< typename CharT, typename Traits, typename Alloc >
-   struct hash< const std::basic_string<CharT, Traits, Alloc> > {
-      size_t operator()(const std::basic_string<CharT, Traits, Alloc>& s) const {
-         
-         const std::collate<CharT>& c = std::use_facet<std::collate<CharT> >(std::locale());
-         return c.hash(s.c_str(), s.c_str() + s.length());
-
-      }
-   };
-
-   template< typename CharT, typename Traits, typename Alloc >
-   struct hash< std::basic_string<CharT, Traits, Alloc> >
-	: hash< const std::basic_string<CharT, Traits, Alloc> > {};
-};
-
-
-typedef __gnu_cxx::hash_map<VertexID, Vertex*> VertexPtrMap;
+typedef std::map<VertexID, Vertex*> VertexPtrMap;
 typedef VertexPtrMap::iterator VertexPtrMapIter;
 typedef VertexPtrMap::const_iterator VertexPtrMapConstIter;
 
