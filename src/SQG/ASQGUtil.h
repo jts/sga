f4b74e45df9e0d2807e2da82040fe07b411524ec
Jared Simpson <js18@sanger.ac.uk>
2010-04-10 17:20:16 +0100

    Added SQG format stub directory/files

---
+++ src/SQG/ASQGUtil.h
@@ -0,0 +1,19 @@
+//-----------------------------------------------
+// Copyright 2010 Wellcome Trust Sanger Institute
+// Written by Jared Simpson (js18@sanger.ac.uk)
+// Released under the GPL
+//-----------------------------------------------
+//
+// ASQGUtil - Routines for parsing an assembly 
+// sequence graph file
+//
+#ifndef ASQGUTIL_H
+#define ASQGUTIL_H
+
+namespace ASQGUtil
+{
+
+
+};
+
+#endif
