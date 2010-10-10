98d1dfc0f474392830ac89a5a03f15bdca3625b6
Jared Simpson <js18@sanger.ac.uk>
2010-10-10 18:02:12 +0100

    Refactored repository to not contain data files and tools/analysis scripts. These are moved to the sgatools repo

--- tools/diffASQG.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-echo $1
-diff -y <(zcat $1 | sort) <(zcat $2 | sort) | grep -A2 -B2 ">\|<"
--- tools/run_evalScaffolds.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-IN=$1
-REF=$2
-TOOLS_DIR=`dirname $0`
-$TOOLS_DIR/breakScaffolds.pl $IN
-$TOOLS_DIR/run_bwasw.sh scaffoldContigs.fa $REF
-python $TOOLS_DIR/evalContigs.py scaffoldContigs.fa.sam | tee -a evalScaf.report
-python $TOOLS_DIR/evalScaffolds.py scaffoldContigs.scaf scaffoldContigs.fa.sam | tee -a evalScaf.report
