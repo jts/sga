6bb3efb6c8fa4ac3cb3809c7c9f68dfabc23b115
Jared Simpson <js18@sanger.ac.uk>
2013-03-12 14:01:55 +0000

    Removed unused var2vcf subprogram

--- src/SGA/sga.cpp
+++ src/SGA/sga.cpp
@@ -31,7 +31,6 @@
 #include "bwt2fa.h"
 #include "graph-diff.h"
 #include "hapgen.h"
-#include "var2vcf.h"
 #include "gapfill.h"
 #include "variant-detectability.h"
 #include "rewrite-evidence-bam.h"
@@ -77,7 +76,6 @@ static const char *SGA_USAGE_MESSAGE =
 "           filterBAM             filter out contaminating mate-pair data in a BAM file\n"
 "           cluster               find clusters of reads belonging to the same connected component in an assembly graph\n"
 //"           connect         resolve the complete sequence of a paired-end fragment\n"
-//"           var2vcf         convert aligned variant sequences found by graph-diff into a VCF file\n"
 //"           hapgen          generate candidate haplotypes from an assembly graph\n"
 "\nReport bugs to " PACKAGE_BUGREPORT "\n\n";
 
@@ -146,8 +144,6 @@ int main(int argc, char** argv)
             bwt2faMain(argc - 1, argv + 1);
         else if(command == "graph-diff")
             graphDiffMain(argc - 1, argv + 1);
-        else if(command == "var2vcf")
-            var2vcfMain(argc - 1, argv + 1);
         else if(command == "hapgen")
             hapgenMain(argc - 1, argv + 1);
         else if(command == "gapfill")
