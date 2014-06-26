6cafe7592ead16ec7d2b38d150f6356f30c59efe
pradosj <julien.prados@gmail.com>
2014-06-26 14:36:20 +0200

    modify to output only canonical kmers, and remove option kmer-threshold

--- src/SGA/kmer-count.cpp
+++ src/SGA/kmer-count.cpp
@@ -15,12 +15,11 @@
 static const char *KMERCOUNT_VERSION_MESSAGE = SUBPROGRAM " Version " PACKAGE_VERSION "\n";
 static const char *KMERCOUNT_USAGE_MESSAGE =
 "Usage: " PACKAGE_NAME " " SUBPROGRAM " [OPTION] ... input.bwt\n"
-"Extract k-mers from a BWT-index.\n"
+"Extract canonical k-mers from a BWT-index, and output a table with the number of occurence of each one on both strand.\n"
 "\n"
 "      --help                           display this help and exit\n"
 "      --version                        display program version\n"
 "      -k, --kmer-size=N                The length of the kmer to use. (default: 27)\n"
-"      -x, --kmer-threshold=N           Output only kmers whose multiplicity is >=N. (default: 1)\n"
 "      -d, --sample-rate=N              use occurrence array sample rate of N in the FM-index. Higher values use significantly\n"
 "                                       less memory at the cost of higher runtime. This value must be a power of 2 (default: 128)\n"
 "\nReport bugs to " PACKAGE_BUGREPORT "\n\n";
@@ -31,7 +30,6 @@ namespace opt {
     static std::string bwtFile;
     static int sampleRate = BWT::DEFAULT_SAMPLE_RATE_SMALL;
     static int kmerLength = 27;
-    static int kmerThreshold = 1;
 }
 
 static const char* shortopts = "d:k:x:";
@@ -39,7 +37,6 @@ enum { OPT_HELP = 1, OPT_VERSION };
 static const struct option longopts[] = {
     { "sample-rate",           required_argument, NULL, 'd' },
     { "kmer-size",             required_argument, NULL, 'k' },
-    { "kmer-threshold",        required_argument, NULL, 'x' },
     { "help",                  no_argument,       NULL, OPT_HELP },
     { "version",               no_argument,       NULL, OPT_VERSION },
     { NULL, 0, NULL, 0 }
@@ -55,7 +52,6 @@ void parseKmerCountOptions(int argc, char** argv) {
         {
             case 'd': arg >> opt::sampleRate; break;
             case 'k': arg >> opt::kmerLength; break;
-            case 'x': arg >> opt::kmerThreshold; break;
             case OPT_HELP:
                 std::cout << KMERCOUNT_USAGE_MESSAGE;
                 exit(EXIT_SUCCESS);
@@ -84,13 +80,6 @@ void parseKmerCountOptions(int argc, char** argv) {
         exit(EXIT_FAILURE);
     }
 
-    if(opt::kmerThreshold <= 0)
-    {
-        std::cerr << SUBPROGRAM ": invalid kmer threshold: " << opt::kmerThreshold << ", must be greater than zero\n";
-        std::cout << "\n" << KMERCOUNT_USAGE_MESSAGE;
-        exit(EXIT_FAILURE);
-    }
-
     opt::bwtFile = argv[optind++];
 }
 
@@ -103,15 +92,17 @@ void parseKmerCountOptions(int argc, char** argv) {
 // Stack structure used in the depth first search of kmers
 struct stack_elt_t
 {
-    size_t strsz;
+    size_t str_sz;
     char bp;
     BWTInterval range;
-    stack_elt_t(size_t strsz,char bp):strsz(strsz),bp(bp){}
-    stack_elt_t(size_t strsz,char bp,const BWTInterval& range):strsz(strsz),bp(bp),range(range){}
+    stack_elt_t(size_t strsz,char bp):str_sz(strsz),bp(bp){}
+    stack_elt_t(size_t strsz,char bp,const BWTInterval& range):str_sz(strsz),bp(bp),range(range){}
 };
 
-// extract all kmers of a bwt
-void traverse_kmer(const BWT* pBWT, unsigned int k, unsigned int kmerThreshold)
+
+
+// extract all kmers of a bwt by performing a backward depth first search
+void traverse_kmer(const BWT* pBWT, unsigned int k)
 {
     std::stack< stack_elt_t > stack;
     std::string str; // string storing the current path
@@ -127,15 +118,28 @@ void traverse_kmer(const BWT* pBWT, unsigned int k, unsigned int kmerThreshold)
     // Perform the kmer search
     while(!stack.empty())
     {
+        // pop an element from the stack, and update the string path accordingly
         stack_elt_t top = stack.top();
         stack.pop();
-        str.resize(top.strsz);
+        str.resize(top.str_sz);
         str.push_back(top.bp);
         if (str.length()>=k) {
-            // we reach a suffix of size k, print it
-            if (top.range.size()>=kmerThreshold)
-            {
-                std::cout << reverse(str) << '\t' << top.range.size() << std::endl;
+            // we found a kmer, retreive the number of occurence
+            std::string seq(reverse(str));
+            int64_t n = top.range.size();
+
+            // look for the count of its reverse complement
+            std::string seq_rc(complement(str));
+            BWTInterval range_rc = BWTAlgorithms::findInterval(pBWT,seq_rc);
+            int64_t n_rc = range_rc.isValid()?range_rc.size():0;
+
+            // print the current kmer if canonical
+            if (seq<seq_rc) {
+                std::cout << seq << '\t' << n << '\t' << n_rc << std::endl;
+            } else if (n_rc<=0) {
+                // the current kmer is not canonical, but the reverse complement doesn't exists
+                // so print it now as it will never be traversed by the searching algorithm
+                std::cout << seq_rc << '\t' << n_rc << '\t' << n << std::endl;
             }
         } else
         {
@@ -161,7 +165,7 @@ int kmerCountMain(int argc, char** argv)
 {
     parseKmerCountOptions(argc,argv);
     BWT* pBWT = new BWT(opt::bwtFile,opt::sampleRate);
-    traverse_kmer(pBWT,opt::kmerLength,opt::kmerThreshold);
+    traverse_kmer(pBWT,opt::kmerLength);
     return 0;
 }
 
